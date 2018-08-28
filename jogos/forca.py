5d7e2bdd5916ab1bb964e1a3d52eac484e533254
Gabriel Oliveira Rozendo <gabriel.rozendo@bh01.local>
2018-08-28 18:15:54 -0300

    Aula 03 finalizada, com algumas melhorias por minha conta

--- jogos/forca.py
+++ jogos/forca.py
@@ -1,28 +1,71 @@
 def jogar():
-    print("*********************************")
-    print("***Bem vindo ao jogo da Forca!***")
-    print("*********************************")
+    tamanho = 50
+    print("*"*tamanho)
+    print("Bem vindo ao jogo da Forca!".center(tamanho))
+    print("*"*tamanho)
+    print()
 
     palavra_secreta = "banana"
     palavra_secreta_upper = palavra_secreta.upper()
+
+    chutes = []
+    mascara = list("_" * len(palavra_secreta))
     enforcou = False
     acertou = False
+    chances = 10
 
     while(not enforcou and not acertou):
-        chute = input("Qual letra? ")
+        print()
+        print(f"Palavra: {''.join(mascara)}")
+        chute = input(f"Tentativa {len(chutes)+1} de {chances}!\tDigite uma letra: ")
         chute = chute.strip()
-        chute_upper = chute.upper()
-        index = 0
+        
+        if len(chute) < 1:
+            print(f"É dificil digitar uma letra?! Tenta de novo aí né...")
+            continue
+        elif len(chute) > 1:
+            print(f"É dificil digitar só uma letra?! :/ vamos ignorar o restante: {chute[1:]}")
+            chute = chute[0]
+        
+        chute_upper = chute.upper()        
+                
+        if chute_upper in chutes:
+            print(f"Você já chutou a letra '{chute}', me ajuda aí né... Quer perder uma tentativa assim fácil?")
+            continue
+        
+        chutes.append(chute_upper)
+
         if chute_upper in palavra_secreta_upper:
+            index = 0
             for letra in palavra_secreta_upper:
-                if(chute_upper == letra):
+                if(chute_upper == letra):                    
                     print(f"Acertou. Letra '{chute}' na posição {index}")
+                    mascara[index] = palavra_secreta[index]
                 index+=1
+            acertou = not "_" in mascara
+
         else:
             print(f"Errou... Não tem '{chute}' na palavra")
-        # print("jogando...")
 
-    print("Fim do jogo")
+        enforcou = len(chutes) == chances
+        
+    print()
+    print("*"*tamanho)
+    if acertou:
+        print("Você ganhou!".upper().center(tamanho))
+        print("Parabéns, você é o máximo!")
+        print(f"Você conseguiu em {len(chutes)} tentativas")
+        if len(chutes)>= 9:
+            print("Foi quase hein... haha")            
+    elif enforcou:
+        print("Você perdeu!".upper().center(tamanho))
+        print("Não desista, quem sabe na próxima...")
+    print()
+    print("*"*tamanho)
+    print("Fim do jogo".center(tamanho))
+
 
 if(__name__ == "__main__"):
     jogar()
+
+
