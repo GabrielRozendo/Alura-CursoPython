189457baae04ec09b8e6e49df334c1ecc54299f6
Gabriel Oliveira Rozendo <gabriel.rozendo@bh01.local>
2018-08-28 21:11:49 -0300

    Aula 07 finalizada

--- jogos/forca.py
+++ jogos/forca.py
@@ -1,14 +1,83 @@
 import random
 
-def jogar():
-    tamanho = 50
-    print("*"*tamanho)
-    print("Bem vindo ao jogo da Forca!".center(tamanho))
-    print("*"*tamanho)
-    print()
+tamanho_tela = 50
 
+
+def jogar():
+    imprimir_cabecalho()    
+
+    palavra_secreta, palavra_secreta_upper = carregar_palavra_secreta()
+
+    chutes = []
+    mascara = inicializar_mascara(palavra_secreta)
+    enforcou = False
+    acertou = False    
+    erros = 0
+    limite_erros = 7
+    while(not enforcou and not acertou):
+        continuar, chute, chute_upper = pedir_chute(mascara, chutes, limite_erros-erros)
+        if continuar:
+            acertou, mascara, erros = verificar_acerto(erros, limite_erros, chute, chute_upper, palavra_secreta, palavra_secreta_upper, mascara)
+        enforcou = erros == limite_erros
+        
+    imprimir_resultado_cabecalho(palavra_secreta)
+    if acertou:        
+        imprimir_mensagem_acertou(chutes)
+    elif enforcou:
+        imprimir_mensagem_perdeu()
+    imprimir_resultado_rodape()
+
+def verificar_acerto(erros, limite_erros, chute, chute_upper, palavra_secreta, palavra_secreta_upper, mascara):
+    if chute_upper in palavra_secreta_upper:
+        index = 0
+        for letra in palavra_secreta_upper:
+            if chute_upper == letra:
+                print(f"Acertou. Letra '{chute}' na posição {index}")
+                mascara[index] = palavra_secreta[index]
+            index+=1
+        acertou = not "_" in mascara
+        return acertou, mascara, erros
+    else:
+        erros+=1
+        desenha_forca(erros)
+        chances_erro = limite_erros - erros
+        plural_vez = "vez" + ("es" if chances_erro > 1 else "")
+        print(f"Errou... Não tem '{chute}' na palavra. Você pode errar apenas mais {chances_erro} {plural_vez}")        
+        return False, mascara, erros
+
+def pedir_chute(mascara, chutes, erros_possiveis):
+    print()
+    print(f"Palavra: {''.join(mascara)}")
+    chute = input(f"Tentativa {len(chutes)+1}!\tErros possíveis {erros_possiveis}\tDigite uma letra: ")
+    chute = chute.strip()
+    continuar, chute, chute_upper = validar_chute(chutes, chute)
+    if not continuar:
+        return False, chute, chute_upper
+    else:
+        chutes.append(chute_upper)
+        return True, chute, chute_upper
+
+def validar_chute(chutes, chute):
+    if len(chute) < 1:
+        print(f"É dificil digitar uma letra?! Tenta de novo aí né...")
+        return False, chute, None
+    elif len(chute) > 1:
+        print(f"É dificil digitar só uma letra?! :/ vamos ignorar o restante: {chute[1:]}")
+        chute = chute[0]
+    chute_upper = chute.upper()
+
+    if chute_upper in chutes:
+        print(f"Você já chutou a letra '{chute}', me ajuda aí né... Quer perder uma tentativa assim fácil?")
+        return False, chute, None
+
+    return True, chute, chute_upper
+
+def inicializar_mascara(palavra_secreta):
+    return list("_" * len(palavra_secreta)) # "_" for letra in palavra_secreta
+
+def carregar_palavra_secreta():
     palavras = []
-    
+
     with open("palavras.txt", "r") as arquivo:
         for linha in arquivo:
             palavras.append(linha.strip())
@@ -16,66 +85,122 @@ def jogar():
     numero_aleatorio = random.randrange(0, len(palavras))
     palavra_secreta = palavras[numero_aleatorio]
 
-    palavra_secreta_upper = palavra_secreta.upper()
+    return palavra_secreta, palavra_secreta.upper()
 
-    chutes = []
-    mascara = list("_" * len(palavra_secreta)) # "_" for letra in palavra_secreta
-    enforcou = False
-    acertou = False
-    chances = len(palavra_secreta) + 6
-
-    while(not enforcou and not acertou):
-        print()
-        print(f"Palavra: {''.join(mascara)}")
-        chute = input(f"Tentativa {len(chutes)+1} de {chances}!\tDigite uma letra: ")
-        chute = chute.strip()
-        
-        if len(chute) < 1:
-            print(f"É dificil digitar uma letra?! Tenta de novo aí né...")
-            continue
-        elif len(chute) > 1:
-            print(f"É dificil digitar só uma letra?! :/ vamos ignorar o restante: {chute[1:]}")
-            chute = chute[0]
-        
-        chute_upper = chute.upper()        
-                
-        if chute_upper in chutes:
-            print(f"Você já chutou a letra '{chute}', me ajuda aí né... Quer perder uma tentativa assim fácil?")
-            continue
-        
-        chutes.append(chute_upper)
-
-        if chute_upper in palavra_secreta_upper:
-            index = 0
-            for letra in palavra_secreta_upper:
-                if(chute_upper == letra):                    
-                    print(f"Acertou. Letra '{chute}' na posição {index}")
-                    mascara[index] = palavra_secreta[index]
-                index+=1
-            acertou = not "_" in mascara
-
-        else:
-            print(f"Errou... Não tem '{chute}' na palavra")
-
-        enforcou = len(chutes) == chances
-        
+def imprimir_cabecalho():
+    imprimir_linha()
+    print("Bem vindo ao jogo da Forca!".center(tamanho_tela))
+    imprimir_linha()
     print()
-    print("*"*tamanho)
-    if acertou:
-        print("Você ganhou!".upper().center(tamanho))
-        print("Parabéns, você é o máximo!")
-        print(f"Você conseguiu em {len(chutes)} tentativas")
-        if len(chutes)>= 9:
-            print("Foi quase hein... haha")            
-    elif enforcou:
-        print("Você perdeu!".upper().center(tamanho))
-        print("Não desista, quem sabe na próxima...")
-    print()
-    print("*"*tamanho)
-    print("Fim do jogo".center(tamanho))
 
+def imprimir_mensagem_perdeu():
+    print("Você perdeu!".upper().center(tamanho_tela))
+    print("Não desista, quem sabe na próxima...")
+    print()
+    imprimir_forca()
+
+def desenha_forca(erros):
+    print("  _______     ")
+    print(" |/      |    ")
+
+    if(erros == 1):
+        print(" |      (_)   ")
+        print(" |            ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erros == 2):
+        print(" |      (_)   ")
+        print(r" |      \     ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erros == 3):
+        print(" |      (_)   ")
+        print(" |      \|    ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erros == 4):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erros == 5):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |            ")
+
+    if(erros == 6):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      /     ")
+
+    if (erros == 7):
+        print(" |      (x)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      / \   ")
+
+    print(" |            ")
+    print("_|___         ")
+    print()
+
+
+def imprimir_trofeu():
+    print("       ___________      ")
+    print("      '._==_==_=_.'     ")
+    print("      .-\\:      /-.    ")
+    print("     | (|:.     |) |    ")
+    print("      '-|:.     |-'     ")
+    print("        \\::.    /      ")
+    print("         '::. .'        ")
+    print("           ) (          ")
+    print("         _.' '._        ")
+    print("        '-------'       ")
+
+def imprimir_forca():
+    print("    _______________         ")
+    print("   /               \       ")
+    print("  /                 \      ")
+    print("//                   \/\  ")
+    print("\|   XXXX     XXXX   | /   ")
+    print(" |   XXXX     XXXX   |/     ")
+    print(" |   XXX       XXX   |      ")
+    print(" |                   |      ")
+    print(" \__      XXX      __/     ")
+    print("   |\     XXX     /|       ")
+    print("   | |           | |        ")
+    print("   | I I I I I I I |        ")
+    print("   |  I I I I I I  |        ")
+    print("   \_             _/       ")
+    print("     \_         _/         ")
+    print("       \_______/           ")
+
+
+def imprimir_mensagem_acertou(chutes):
+    print("Você ganhou!".upper().center(tamanho_tela))
+    print("Parabéns, você é o máximo!")
+    print(f"Você conseguiu em {len(chutes)} tentativas")
+    if len(chutes)>= 9:
+        print("Foi quase hein... haha")
+    imprimir_trofeu()
+
+def imprimir_resultado_rodape():
+    print()
+    imprimir_linha()
+    print("Fim do jogo".center(tamanho_tela))
+
+def imprimir_linha():
+    print("*" * tamanho_tela)
+
+def imprimir_resultado_cabecalho(palavra_secreta):
+    print()
+    print(palavra_secreta.upper().center(tamanho_tela))
+    imprimir_linha()
 
 if(__name__ == "__main__"):
     jogar()
-
-
